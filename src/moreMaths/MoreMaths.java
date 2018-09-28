3690d49223bfd00123faa6569f83ed0ab56bddc8
koldointxaustiusername <ik013043z1@H013043.NET>
2018-09-28 11:34:58 +0200

    not finished

---
+++ src/moreMaths/MoreMaths.java
@@ -0,0 +1,18 @@
+package moreMaths;
+
+public class MoreMaths {
+	public static void main (String[] arg) {
+		int a = 5;
+		// a++ to sum 1
+		a++;
+		// a*=2 to multiplicate with 2
+		a*=2;
+		// a-=3 to subtract 3
+		a-=3;
+		// a%=5 to know the numbers we left when we subtract 5
+		a%=5;
+		// to sum 7 to the result
+		a=a+7;
+		System.out.println(a);
+	}
+}
